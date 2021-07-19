cff5b830a51e6a5b5eae352c6ed3b5e4cadcf3ef
sxweb <phpred@yandex.ru>
2021-07-19 08:34:56 +0300

    added auth, product, review, top-page controllers

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import {Body, Controller, HttpCode, Post} from '@nestjs/common';
+import {AuthDto} from "./dto/auth.dto";
+
+@Controller('auth')
+export class AuthController {
+    @Post('register')
+    async register(@Body() dto:AuthDto){
+
+    }
+    @HttpCode(200)
+    async login(@Body() dto:AuthDto){
+
+    }
+}
